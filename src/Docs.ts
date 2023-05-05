2269942f9e0cb2697998085c6451431e1cfbb1eb
Cat++ <69035887+NotGhex@users.noreply.github.com>
2023-05-05 16:17:56 +0800

    test

---
+++ src/Docs.ts
@@ -0,0 +1,103 @@
+import { Collection, SlashCommandSubcommandBuilder } from 'discord.js';
+import { BaseModule } from './BaseModule.js';
+import { RecipleClient, RecipleModule, SlashCommandBuilder } from 'reciple';
+import DocsParser from './DocsParser.js';
+import { DocElement } from 'discord.js-docs';
+import { limitString } from 'fallout-utility';
+
+export class Docs extends BaseModule {
+    public async onStart(client: RecipleClient<false>, module: RecipleModule): Promise<boolean> {
+        this.commands = [
+            new SlashCommandBuilder()
+                .setName('docs')
+                .setDescription('Display docs for certain package')
+                .addSubcommand(reciple => this.subcommandOptions(reciple)
+                    .setName('reciple')
+                    .setDescription('Display docs for reciple')
+                )
+                .addSubcommand(reciple => this.subcommandOptions(reciple)
+                    .setName('client')
+                    .setDescription('Display docs for @reciple/client')
+                )
+                .setDMPermission(true)
+                .setExecute(async ({ interaction }) => {
+                    const subcommand = interaction.options.getSubcommand() as 'client'|'reciple';
+                    const query = interaction.options.getString('query', true).replace('()', '');
+                    const hidden = interaction.options.getBoolean('hide') || false;
+                    const target = interaction.options.getUser('target');
+                    const docs = DocsParser.getDocsData(subcommand);
+
+                    const [parentQuery, childQuery] = (query.includes('#')
+                        ? query.split('#')
+                        : query.includes('.')
+                            ? query.split('.')
+                            : [query]) as [string|undefined, string|undefined];
+
+                    const parent: DocElement|undefined = (parentQuery ? docs.get(parentQuery) : null) || undefined;
+                    const child: DocElement|undefined = (childQuery && parent ? new Collection<string, DocElement>(parent.children.entries()).find(d => d.name === childQuery) : null) || undefined;
+                    const data = (child || parent)!;
+                    const name = parent!.name + (child ? ('#' + child.name) : '');
+
+                    if (!parent || childQuery && !child) {
+                        await interaction.reply({ content: `No results found`, ephemeral: true });
+                        return;
+                    }
+
+                    const docsUrl = child ? DocsParser.createDocsLink(child, parent) : DocsParser.createDocsLink(parent);
+                    const targetMessage = target && !hidden ? `*Documentation suggestion for ${target}*\n` : '';
+
+                    await interaction.reply({
+                        content: `${targetMessage + DocsParser.getTypeEmoji(data.docType)} [__**${name}**__](<${docsUrl}>)\n${limitString(data.description ?? '', 1800)}`,
+                        ephemeral: hidden
+                    });
+                })
+        ];
+
+        return true;
+    }
+
+    public async onLoad(client: RecipleClient<true>, module: RecipleModule): Promise<void> {
+        client.on('interactionCreate', async interaction => {
+            if (!interaction.isAutocomplete()) return;
+
+            const command = interaction.commandName;
+            const subcommand = interaction.options.getSubcommand() as 'client'|'reciple';
+            const option = interaction.options.getFocused(true);
+            const query = option.value.trim();
+
+            if (command !== 'docs' && option.name !== 'query') return;
+
+            const docs = DocsParser.getDocsData(subcommand);
+            const results = docs.search(query, { excludePrivateElements: true }) || [];
+
+            await interaction.respond(
+                results.map(r => {
+                    return {
+                        name: r.formattedName,
+                        value: r.formattedName
+                    };
+                })
+            );
+        });
+    }
+
+    public subcommandOptions(subcommand: SlashCommandSubcommandBuilder): SlashCommandSubcommandBuilder {
+        return subcommand
+            .addStringOption(query => query
+                .setName('query')
+                .setDescription('Search the docs for classes, functions, or typedefs')
+                .setRequired(true)
+                .setAutocomplete(true)
+            )
+            .addUserOption(target => target
+                .setName('target')
+                .setDescription('User to mention')
+            )
+            .addBooleanOption(hide => hide
+                .setName('hide')
+                .setDescription('Hide command response')
+            );
+    }
+}
+
+export default new Docs();
---
+++ src/classes/DocTagManager.ts
@@ -0,0 +1,68 @@
+import { BaseManager, Collection } from 'discord.js';
+import { DocsParser } from '../DocsParser.js';
+import axios from 'axios';
+import Doc from 'discord.js-docs';
+
+export interface APIGitHubContentPartial {
+    name: string;
+    path: string;
+    sha: string;
+    size: number;
+    url: string;
+    html_url: string;
+    git_url: string;
+    download_url: string;
+    type: "file"|"dir";
+}
+
+export interface DocTag<HasContent extends boolean = boolean> {
+    tag: string;
+    jsonUrl: string;
+    content: HasContent extends true
+        ? Doc.default
+        : HasContent extends false
+            ? null
+            : Doc.default|null;
+}
+
+export class DocTagManager extends BaseManager {
+    readonly cache: Collection<string, DocTag> = new Collection();
+
+    constructor(readonly pkg: string, readonly parser: DocsParser) {
+        super(parser.client);
+    }
+
+    public async fetch(options?: { cache?: boolean; fetchContent?: boolean; }): Promise<DocTag[]> {
+        const url = `https://api.github.com/repos/FalloutStudios/Reciple/contents/${this.pkg}?ref=docs`;
+        const raw = await axios.get<APIGitHubContentPartial[]>(url, { responseType: 'json' });
+        const data = await Promise.all(raw.data.map(async r => {
+            return {
+                tag: r.name.replace('.json', ''),
+                jsonUrl: `https://raw.githubusercontent.com/FalloutStudios/Reciple/docs/${r.path}`,
+                content: options?.fetchContent !== false
+                    ? await this.fetchContent(r.name.replace('.json', '')) ?? null
+                    : null
+            } satisfies DocTag;
+        }));
+
+        if (options?.cache !== false) {
+            for (const tag of data) {
+                this.cache.set(tag.tag, tag);
+            }
+        }
+
+        return data;
+    }
+
+    public async fetchContent(tag: string|DocTag): Promise<Doc.default|undefined> {
+        const jsonUrl = typeof tag === 'string' ? this.cache.get(tag) : tag;
+        if (!jsonUrl) return;
+
+        const url = jsonUrl.jsonUrl;
+        const doc = await Doc.default.fetch(url, { force: true });
+
+        jsonUrl.content = doc;
+
+        return doc;
+    }
+}
